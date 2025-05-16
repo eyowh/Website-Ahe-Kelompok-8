0b8ff5c822febbdfa48c935cc1209fba7e7273c7
eyowh <ario.ardhi2eeeee@gmail.com>
2025-05-17 00:05:00 +0700

    Update halaman admin

--- profil/admin.py
+++ profil/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Artikel
+from .models import Artikel, Program, KelolaGaleri
 
-admin.site.register(Artikel)
+@admin.register(Artikel)
+class ArtikelAdmin(admin.ModelAdmin):
+    list_display = ('judul', 'halaman', 'tanggal')
+    list_filter = ('halaman', 'tanggal')
+    search_fields = ('judul', 'isi')
+    prepopulated_fields = {'slug': ('judul',)}
+    ordering = ['-tanggal']
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('nama',)
+    search_fields = ('nama', 'deskripsi')
+
+@admin.register(KelolaGaleri)
+class KelolaGaleriAdmin(admin.ModelAdmin):
+    list_display = ('caption',)
+    search_fields = ('caption',)
